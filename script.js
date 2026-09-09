288a229788ecd9016b01820c5b399ebf79dfd16f
cooper case <coopercase20@gmail.com>
2026-09-08 21:59:08 -0400

    20

--- script.js
+++ script.js
@@ -142,7 +142,7 @@ document.getElementById("unfinished-toggle").addEventListener("click", () => {
     const arrow = document.querySelector("#unfinished-toggle .arrow");
     const isClosed = current === "none";
 
-    arrow.classList.toggle("closed", isClosed);
+    arrow.classList.toggle("closed", !isClosed);
     box.style.display = current === "none" ? "block" : "none";
 });
 
@@ -152,7 +152,7 @@ document.getElementById("finished-toggle").addEventListener("click", () => {
 
     const arrow = document.querySelector("#finished-toggle .arrow");
     const isClosed = current === "none";
-    
-    arrow.classList.toggle("closed", isClosed);
+
+    arrow.classList.toggle("closed", !isClosed);
     box.style.display = current === "none" ? "block" : "none";
 });
